11632d533a26d53c084edd371fbfad4781f6141c
sanket <sanket333deshmukh@gmail.com>
2023-12-14 11:26:58 +0530

    Added some Chrome Option

---
+++ SeleniumDemo/src/test/java/com/sslcertificationcheck/SSLCheck.java
@@ -0,0 +1,18 @@
+package com.sslcertificationcheck;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SSLCheck {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options= new ChromeOptions();
+		options.setAcceptInsecureCerts(true);
+		WebDriver driver= new ChromeDriver(options);
+		driver.get("https://expired.badssl.com/");
+		System.out.println(driver.getTitle());		
+	}
+}
